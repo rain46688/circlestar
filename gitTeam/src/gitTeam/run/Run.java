78229206e051ee280440610faa575d8e22425563
rain46688 <rain46688@gmail.com>
2020-09-03 21:27:08 +0900

    cms 0903

--- gitTeam/src/gitTeam/run/Run.java
+++ gitTeam/src/gitTeam/run/Run.java
@@ -1,5 +1,9 @@
 package gitTeam.run;
 
-public class Run {
+import gitTeam.view.Cms;
 
+public class Run {
+	public static void main(String[] args) {
+		Cms cms = new Cms();
+	}
 }
--- gitTeam/src/gitTeam/view/Jeong.java
+++
@@ -1,5 +0,0 @@
-package gitTeam.view;
-
-public class Jeong {
-
-}
