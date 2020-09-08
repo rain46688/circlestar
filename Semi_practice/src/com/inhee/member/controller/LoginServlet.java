9ce4a151b2dda9b33f8a1588fbbb03605dc008fb
INHEE-KANG <ssrddd@naver.com>
2020-09-08 22:02:20 +0900

    jusuk

---
+++ Semi_practice/src/com/inhee/member/controller/LoginServlet.java
@@ -0,0 +1,72 @@
+package com.inhee.member.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.inhee.member.model.service.MemberService;
+import com.inhee.member.model.vo.Member;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String memberId=request.getParameter("memberId");
+		String memberPwd=request.getParameter("memberPwd");
+		Member m=new MemberService().loginMember(memberId,memberPwd);
+		
+		String saveId=request.getParameter("saveId");
+		
+		if(saveId!=null) {
+			Cookie c=new Cookie("saveId",memberId);
+			c.setMaxAge(7*24*60*60);//일주일간 저장할거임
+			response.addCookie(c);
+		}else {
+			Cookie c=new Cookie("saveId","");
+			c.setMaxAge(0);
+			response.addCookie(c);
+		}
+		
+		if(m!=null) {
+			HttpSession session=request.getSession();
+			session.setAttribute("loginnedMember",m);
+			response.sendRedirect(request.getContextPath());
+		}else {
+			request.setAttribute("msg", "아이디나 비밀번호가 일치하지 않습니다!");
+			request.setAttribute("loc", "/");
+			RequestDispatcher rd=request.getRequestDispatcher("/views/common/msg.jsp");
+			rd.forward(request,response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
