8d9c1737e16b314d4e56f7251ac5ab8c3144b7e3
INHEE-KANG <ssrddd@naver.com>
2020-09-08 22:37:36 +0900

    지금까지 한거!

---
+++ nbbang/src/com/nbbang/member/model/service/MemberService.java
@@ -0,0 +1,21 @@
+package com.nbbang.member.model.service;
+
+import static com.nbbang.common.temp.JDBCTemplate.close;
+import static com.nbbang.common.temp.JDBCTemplate.getConnection;
+
+import java.sql.Connection;
+
+import com.nbbang.member.model.dao.MemberDao;
+import com.nbbang.member.model.vo.Member;
+
+
+public class MemberService {
+private MemberDao dao=new MemberDao();
+	
+	public Member loginMember(String memberId, String memberPwd) {
+		Connection conn=getConnection();
+		Member m=dao.loginMember(conn, memberId, memberPwd);
+		close(conn);
+		return m;
+	}
+}
--- nbbang/web/views/common/header.jsp
+++ nbbang/web/views/common/header.jsp
@@ -1,9 +1,10 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ page import="com.nbbang.member.model.vo.Member" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
-
+<% Member loginnedMember=(Member)session.getAttribute("loginnedMember");%>
 <!DOCTYPE html>
 <html>
 <head>
