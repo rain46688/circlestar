af4a563f5315643b2153ea066e1614434064444c
rain46688 <rain46688@gmail.com>
2020-09-08 22:17:20 +0900

    cms 0908

--- nbbang/src/com/nbbang/admin/controller/AdminMemberListServlet.java
+++ nbbang/src/com/nbbang/admin/controller/AdminMemberListServlet.java
@@ -27,7 +27,8 @@ public class AdminMemberListServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+		
 	}
 
 	/**
