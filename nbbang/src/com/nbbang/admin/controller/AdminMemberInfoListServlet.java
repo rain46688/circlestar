fdfc06cde8698a8cfd9b062b6f13ee9e584b70bc
rain46688 <rain46688@gmail.com>
2020-09-26 20:54:12 +0900

    어드민 부분 구현중 cms 0926

--- nbbang/src/com/nbbang/admin/controller/AdminMemberInfoListServlet.java
+++ nbbang/src/com/nbbang/admin/controller/AdminMemberInfoListServlet.java
@@ -2,13 +2,15 @@ package com.nbbang.admin.controller;
 
 import java.io.IOException;
 import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.nbbang.admin.model.AdminMem;
 import com.nbbang.admin.model.service.AdminService;
-import com.nbbang.customer.model.vo.CustomerCenter;
 
 /**
  * Servlet implementation class AdminMemberInfoServlet
@@ -31,7 +33,7 @@ public class AdminMemberInfoListServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		int cPage;
-		List<CustomerCenter> list =null;
+		List<AdminMem> list =null;
 		try {
 			cPage = Integer.parseInt(request.getParameter("cPage"));
 		} catch (NumberFormatException e) {
--- nbbang/src/com/nbbang/admin/model/service/AdminService.java
+++ nbbang/src/com/nbbang/admin/model/service/AdminService.java
@@ -6,6 +6,7 @@ import static com.nbbang.common.temp.JDBCTemplate.getConnection;
 import java.sql.Connection;
 import java.util.List;
 
+import com.nbbang.admin.model.AdminMem;
 import com.nbbang.admin.model.dao.AdminDao;
 import com.nbbang.customer.model.vo.CustomerCenter;
 
@@ -56,30 +57,13 @@ public class AdminService {
 		return cnt;
 	}
 
-//	public List<Member> selectMemberSearch(String type, String keyword, int cPage, int numPerPage) {
-//		// TODO Auto-generated method stub
-//		Connection conn = getConnection();
-//		List<Member> list = dao.selectMemberSearch(conn, type, keyword, cPage, numPerPage);
-//		close(conn);
-//		return list;
-//	}
-//
-//
-//
-//	public int selectMemberSearchCount(String type, String keyword) {
-//		// TODO Auto-generated method stub
-//		Connection conn = getConnection();
-//		int cnt = dao.selectMemberSearchCount(conn, type, keyword);
-//		close(conn);
-//		return cnt;
-//	}
-//
-//	public Member memstatuspage(String userid) {
-//		// TODO Auto-generated method stub
-//		Connection conn = getConnection();
-//		Member m = dao.memstatuspage(conn, userid);
-//		close(conn);
-//		return m;
-//	}
+	public List<AdminMem> memberInfoList(int cPage, int numPerPage) {
+		// TODO Auto-generated method stub
+		Connection conn = getConnection();
+		List<AdminMem> list = aa.memberInfoList(conn, cPage, numPerPage);
+		close(conn);
+		return list;
+	}
+
 
 }
