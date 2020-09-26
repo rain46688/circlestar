eba8bb779a5185c41495f041717d00ac6e73fb5a
INHEE-KANG <ssrddd@naver.com>
2020-09-27 00:40:01 +0900

    집가기전에 -inhee- 09270039

--- nbbang/src/com/nbbang/member/controller/ReportPageServlet.java
+++ nbbang/src/com/nbbang/member/controller/ReportPageServlet.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class ReportMember
  */
 @WebServlet("/member/report")
-public class ReportMember extends HttpServlet {
+public class ReportPageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ReportMember() {
+    public ReportPageServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,7 +27,10 @@ public class ReportMember extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		int usid=Integer.parseInt(request.getParameter("userId"));
+		int boardId=Integer.parseInt(request.getParameter("boardId"));
+		int boardWriterUsid=Integer.parseInt(request.getParameter("writerUsid"));
+		System.out.println("usid"+usid+"bid"+boardId+"writer"+boardWriterUsid);
 	}
 
 	/**
