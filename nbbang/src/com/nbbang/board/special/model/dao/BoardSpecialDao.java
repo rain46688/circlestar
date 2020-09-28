4777e615b249b02ee611b20fef4b5c15dccb5a7b
rain46688 <rain46688@gmail.com>
2020-09-29 00:05:31 +0900

    특가 만드는 중 8 cms 0929

--- nbbang/src/com/nbbang/board/special/model/dao/BoardSpecialDao.java
+++ nbbang/src/com/nbbang/board/special/model/dao/BoardSpecialDao.java
@@ -8,12 +8,13 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Properties;
-
 import com.nbbang.board.model.vo.Board;
 import com.nbbang.board.model.vo.BoardFile;
 import com.nbbang.board.model.vo.Card;
@@ -116,6 +117,7 @@ public class BoardSpecialDao {
 				c.getCardBoard().setLikeCount(rs.getInt("LIKE_COUNT"));
 				Calendar ca = new GregorianCalendar();
 				c.getCardBoard().setLimitTime(rs.getTimestamp("LIMIT_TIME", ca));
+				System.out.println("list dao : "+c.getCardBoard().getLimitTime());
 				try {
 					c.getCardBoard().setTradeArea(AESCrypto.decrypt(rs.getString("TRADE_AREA")));
 				} catch (Exception e) {
@@ -136,11 +138,91 @@ public class BoardSpecialDao {
 		return list;
 	}
 	
+	public Card boardPage(Connection conn, String boardId) {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = prop.getProperty("boardPage");
+		System.out.println(sql);
+		Card c = null;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, boardId);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				c = new Card(new Board(), new BoardFile());
+				c.getCardBoard().setBoardId(rs.getInt("BOARD_ID"));
+				c.getCardBoard().setBoardTitle(rs.getString("BOARD_TITLE"));
+				c.getCardBoard().setWriterUsid(rs.getInt("WRITER_USID"));
+				c.getCardBoard().setWriterNickname(rs.getString("WRITER_NICKNAME"));
+				c.getCardBoard().setContent(rs.getString("CONTENT"));
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				java.util.Date enrollDate = sdf.parse(rs.getTimestamp(("ENROLL_DATE")).toString().substring(0, 19));
+				c.getCardBoard().setTradeKind(rs.getString("TRADE_KIND"));
+				c.getCardBoard().setEnrollDate(enrollDate);
+				Calendar ca = new GregorianCalendar();
+				c.getCardBoard().setLimitTime(rs.getTimestamp("LIMIT_TIME", ca));
+				System.out.println("리미트 타임 : "+c.getCardBoard().getLimitTime());
+				c.getCardBoard().setHit(rs.getInt("HIT"));
+				c.getCardBoard().setLikeCount(rs.getInt("LIKE_COUNT"));
+				c.getCardBoard().setProductCategory(rs.getString("PRODUCT_CATEGORY"));
+				try {
+					c.getCardBoard().setTradeArea(new AESCrypto().decrypt(rs.getString("TRADE_AREA")));
+				} catch (Exception e) {
+					c.getCardBoard().setTradeArea(rs.getString("TRADE_AREA"));
+				}
+				c.getCardBoard().setMaxMems(rs.getInt("MAX_MEMS"));
+				/* c.getCardBoard().setLimitTime(rs.getDate("LIMIT_TIME")); */
+				c.getCardBoard().setTradeStage(rs.getInt("TRADE_STAGE"));
+				c.getCardBoard().setPopularBoard(rs.getBoolean("POPULAR_BOARD"));
+				c.getCardBoard().setProductPrice(rs.getInt("PRODUCT_PRICE"));
+				c.getCardBoard().setOwnStatus(rs.getString("TRADE_KIND"));
+				c.getCardBoard().setProductUrl(rs.getString("PRODUCT_URL"));
+				c.getCardFile().setFileName(stringToArr(rs.getString("FILE_NAME")));
+			}
+		} catch (SQLException | ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			close(rs);
+			close(pstmt);
+		}
+		return c;
+	}
 	
+	public Card boardPageForProfile(Connection conn, Card c, int writerUsid) {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = prop.getProperty("boardPageForProfile");
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, writerUsid);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				c.setWriterProfile(rs.getString(1));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			close(rs);
+			close(pstmt);
+		}
+		return c;
+	}
 	
-	
-	
-	
+	public int updateReadCount(Connection conn, int boardId) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		try {
+			pstmt = conn.prepareStatement(prop.getProperty("updateReadCount"));
+			pstmt.setInt(1, boardId);
+			result = pstmt.executeUpdate();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}return result;
+	}
 	
 	
 	
@@ -161,6 +243,22 @@ public class BoardSpecialDao {
 				return arr;
 			}
 		}
+	}
+
+	public int tradeClose(Connection conn, int boardId) {
+		// TODO Auto-generated method stub
+		PreparedStatement pstmt = null;
+		int result = 0;
+		try {
+			pstmt = conn.prepareStatement(prop.getProperty("tradeClose"));
+			pstmt.setInt(1, boardId);
+			result = pstmt.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(pstmt);
+		}
+		return result;
 	}
 	
 	
--- nbbang/src/com/nbbang/board/special/model/service/BoardSpecialService.java
+++ nbbang/src/com/nbbang/board/special/model/service/BoardSpecialService.java
@@ -8,10 +8,13 @@ import static com.nbbang.common.temp.JDBCTemplate.rollback;
 import java.sql.Connection;
 import java.util.List;
 
+import com.nbbang.board.model.dao.BoardDao;
 import com.nbbang.board.model.vo.Board;
 import com.nbbang.board.model.vo.BoardFile;
 import com.nbbang.board.model.vo.Card;
 import com.nbbang.board.special.model.dao.BoardSpecialDao;
+import com.nbbang.chat.model.dao.ChatDao;
+import com.nbbang.chat.model.service.ChatService;
 
 public class BoardSpecialService {
 
@@ -40,7 +43,58 @@ public class BoardSpecialService {
 	}
 	
 	
-	
+	public Card boardPage(String boardId, boolean hasRead, int writerUsid) {
+		Connection conn = getConnection();
+		Card c = dao.boardPage(conn, boardId);
+		c = dao.boardPageForProfile(conn, c, writerUsid);
+		if (c != null && !hasRead) {
+			int result = dao.updateReadCount(conn, Integer.parseInt(boardId));
+			if (result > 0)
+				commit(conn);
+			else
+				rollback(conn);
+		}
+		close(conn);
+		return c;
+	}
+
+
+	public int addMem(int boardId, int userUsid, String loginNickname) {
+		// TODO Auto-generated method stub
+		Connection conn = getConnection();
+		int maxmem = new ChatService().getMaxMems(""+boardId);
+		int curmem = new ChatService().getCurMems(""+boardId);
+		int result = 0;
+		if(curmem == maxmem) {
+			close(conn);
+			return 4;
+			//특가 인원꽉참
+		}
+		
+		if(curmem+1 >= maxmem) {
+			//추가 하고 마감시키기
+			result = new BoardDao().boardInsertTradeList(conn, userUsid, loginNickname);
+			System.out.println("특가 인원 추가함");
+			if(result > 0) {
+				//마감시키기
+				result = dao.tradeClose(conn,boardId);
+				System.out.println("특가 인원 추가 하고 마감됨");
+			}else {
+				result = 0;
+			}
+			
+		}else {
+			//그냥 추가하기
+			result = new BoardDao().boardInsertTradeList(conn, userUsid, loginNickname);
+			System.out.println("특가 인원 추가만 함 마감은 안됨");
+		}
+		
+		if(result > 0) commit(conn);
+		else rollback(conn);
+		
+		return result;
+		
+	}
 	
 	
 	
